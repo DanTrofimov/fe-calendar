d6de2e113731069949d1a570e22e63123f4176a4
Daniil Trofimov <trofimovdanil946@gmail.com>
2022-02-10 02:04:15 +0300

    setup lint

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,8 @@ import React from "react";
 import style from "./App.module.css";
 
 function App() {
-  const sample = <p>hello</p>;
-
   return (
     <div className={style.App}>
-      {sample}
       <img src="logo512.png" alt="logo" />
     </div>
   );
