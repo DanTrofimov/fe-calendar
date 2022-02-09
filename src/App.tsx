c2ea5d4eea088cac6e8de4dfcf89fbc638f1cf75
Daniil Trofimov <trofimovdanil946@gmail.com>
2022-02-10 01:39:19 +0300

    added linter & prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <img src="logo512.png" alt="logo" />
+      <img src="logo512.png" alt="logo" />
     </div>
   );
 }
