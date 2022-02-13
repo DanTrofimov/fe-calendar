46a1337d24992be4327a8a640807155280b601d8
Daniil Trofimov <trofimovdanil946@gmail.com>
2022-02-14 01:53:22 +0300

    update fetch opts

--- src/store/user/thunks.ts
+++ src/store/user/thunks.ts
@@ -3,6 +3,11 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 export const getUserThunk = createAsyncThunk("user/getUser", async () => {
   const response = await fetch(`${process.env.REACT_APP_API_URL}/user`, {
     method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Cache: "no-cache"
+    },
     credentials: "include"
   });
   const data = await response.json();
