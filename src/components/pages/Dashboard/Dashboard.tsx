ec8c860e8a342ff5075cb9b0bfb495c28ebc06a1
Daniil Trofimov <trofimovdanil946@gmail.com>
2022-02-15 19:24:09 +0300

    added year select

--- src/components/pages/Dashboard/Dashboard.tsx
+++ src/components/pages/Dashboard/Dashboard.tsx
@@ -6,23 +6,33 @@ import { getEventsThunk } from "../../../store/events/thunks";
 import { getUserThunk } from "../../../store/user/thunks";
 import { Event, User } from "../../../domain";
 import EventsCalendar from "../../organisms/EventsCalendar";
+import YearSelect from "../../atoms/YearSelect";
 import styles from "./styles.module.css";
 
 const Dashboard: FC = () => {
   const dispatch = useDispatch();
-  const [year, setYear] = useState(2021);
+  const [year, setYear] = useState(new Date().getFullYear().toString());
 
   useEffect(() => {
     dispatch(getUserThunk());
-    dispatch(getEventsThunk(year.toString()));
+    dispatch(getEventsThunk(year));
   }, [dispatch, year]);
+
+  const years = ["2016", "2017", "2018", "2019", "2020", "2021", "2022"];
 
   const user: User | null = useSelector(selectUser);
   const events: Event[] = useSelector(selectEvents);
 
   return (
     <div className={styles["calendar-container"]}>
-      <h2 className="email">{user?.email}</h2>
+      <div className={styles["year-select-container"]}>
+        <YearSelect
+          options={years}
+          value={year}
+          setValue={setYear}
+          label="Current year"
+        />
+      </div>
       <EventsCalendar year={year.toString()} events={events} />
     </div>
   );
