7ad46f4801b9f06108f8e3271c21654624ee2caa
Daniil Trofimov <trofimovdanil946@gmail.com>
2022-02-18 03:44:52 +0300

    added content

--- src/components/pages/EventListRequests/EventListRequests.tsx
+++ src/components/pages/EventListRequests/EventListRequests.tsx
@@ -1,26 +1,30 @@
-import React, {FC, useEffect} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import React, { FC, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "../../molecules/Header";
 import styles from "../Dashboard/styles.module.css";
-import {getRequestThunk} from "../../../store/requests/thunks";
-import {selectRequests} from "../../../store/requests/selectors";
-import {Request} from "../../../domain";
-import {Routes} from "../../../constants/routes";
+import { getRequestThunk } from "../../../store/requests/thunks";
+import { selectRequests } from "../../../store/requests/selectors";
+import { Request } from "../../../domain";
+import { Routes } from "../../../constants/routes";
 
 const EventListRequests: FC = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getRequestThunk());
-  }, [dispatch])
-  
+  }, [dispatch]);
+
   const requests: Request[] | null = useSelector(selectRequests);
-  
+
   console.log(requests);
   return (
     <div className={styles["calendar-container"]}>
-      <Header buttonTitle='Dashboard' buttonRouter={Routes.DASHBOARD} />
-      <h1>Requested events</h1>
-    </div>)}
+      <Header buttonTitle="Dashboard" buttonRouter={Routes.DASHBOARD} />
+      <div className={styles.content}>
+        <h1>Requested events</h1>
+      </div>
+    </div>
+  );
+};
 
 export default EventListRequests;
